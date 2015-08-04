01b7f48a6ecdf9a38ff71f3eb9260d2fe8690aaf
HulaSamsquanch <samsquanchhunter14@gmail.com>
2015-08-03 19:09:34 -0500

    Create myoot.user.js

---
+++ myoot.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        myo͞ot
+// @description Prevents any stray audio clips from automatically playing on AIM games.
+// @namespace   kaffeinition@gmail.com
+// @include     http://aimgames.forummotion.com/*
+// @version     1.224
+// @icon        http://www.mediafire.com/convkey/c313/jnx13q9ha6j01w9zg.jpg
+// @updateURL   https://openuserjs.org/install/AIMGamesDaler/myo͞ot.user.js      
+// ==/UserScript==
+
+var audio_players = document.getElementsByTagName("audio");
+var loaded = false;
+
+var clips_stopped = 0;
+
+if(!loaded ){
+    for(var mute = 0; mute < audio_players.length; mute++){
+        audio_players[mute].muted = 1;
+        audio_players[mute].loop = false;
+        clips_stopped++;
+    }
+}
+
+window.onload =          
+    function()   {
+        loaded = true;
+        console.log(clips_stopped + " clips stopped!");
+        if(clips_stopped == 1){
+            window.alert(clips_stopped + " clips stopped!\nThe URL for the clip is: " + audio_players[0].currentSrc);
+        }
+        if(clips_stopped > 1){
+            window.alert(clips_stopped + " clips stopped!\nToo many sources to list");
+        }
+};
