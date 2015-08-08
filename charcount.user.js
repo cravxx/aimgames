a2356a24323aadc545446a159c38f0c6829f5703
Daffeinatek <samsquanchhunter14@gmail.com>
2015-08-08 00:15:22 -0500

    creation of charcount
    soon to be included within swearify, this script utilizes Countable, an easy way to get the character count of an element.

---
+++ charcount.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name        Charcount
+// @namespace   kaff_is_one_grease
+// @include     http://aimgames.forummotion.com/*
+// @version     1
+// @grant       none
+// @require     https://raw.githubusercontent.com/RadLikeWhoa/Countable/master/Countable.js
+// ==/UserScript==
+
+var cssLabel = "color: grey;font-size: 12px;";
+
+var location = "";
+var refined_loc = "";
+var cssTd = "";
+
+var preview = false;
+
+function values(o) {
+  return Object.keys(o).map(function (k) {
+    return o[k]
+  })
+}
+
+window.onload = function()   {
+  if (typeof document.getElementsByTagName("textarea")[1] === 'undefined') { ////PREVIEW
+    preview = true;
+    location = document.getElementById("parent_editor_simple").getElementsByClassName("row2")[0];
+    refined_loc = document.getElementById("parent_editor_simple").getElementsByClassName("row2")[0];
+  }else{    
+    location = document.getElementById("quick_reply").getElementsByClassName("row2")[1];  
+    cssTd = "padding-top:5px;";
+
+    var new_td = document.createElement("td");
+    location.appendChild(new_td).style.cssText = cssTd;
+    refined_loc = document.getElementById("quick_reply").getElementsByClassName("row2")[1].getElementsByTagName("td")[0];
+  }    
+  var element = document.createElement("label");
+  refined_loc.appendChild(element).style.cssText = cssLabel;
+};
+
+setInterval(function () {      
+  var area = "";  
+  if(!preview){
+    area = document.getElementsByTagName("textarea")[0];
+    area.value = document.getElementsByTagName("textarea")[1].value;
+  }else{
+    area = document.getElementsByTagName("textarea")[0];
+  }
+  Countable.once(area, function (counter) {
+    location.getElementsByTagName("label")[0].innerHTML = values(counter)[4] + " characters";
+    console.log(values(counter)[4]);
+    if(values(counter)[4] > 32000){
+      console.log("got here");
+      element.style.cssText += "color:red;";
+    }else if(values(counter)[4] < 32000){
+      element.style.cssText = cssLabel;
+    }
+  });    
+}, 3000);
