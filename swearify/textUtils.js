b6a90a8327d0c7be264c0b23805ffd1b72f63c61
rafa1231518 <chris331hansen@outlook.com>
2015-08-15 20:32:49 -0300

    add javascript syntax highlighting

--- swearify/textUtils.js
+++ swearify/textUtils.js
@@ -13,8 +13,71 @@ function Rainbow(){"use strict";function e(e){if(e.length<2)throw new Error("Rai
 // nicer colors
 // var ColorList = ["#FF0000", "#FF8B00", "#E7FF00", "#5CFF00", "#00FF2E", "#00FFB9", "#00B9FF", "#002EFF", "#5C00FF", "#E700FF", "#FF008B"];
 // even nicer colors
-var ColorList = ["#F0F8FF", "#FAEBD7", "#00FFFF", "#7FFFD4", "#F0FFFF", "#F5F5DC", "#FFE4C4", "#000000", "#FFEBCD", "#0000FF", "#8A2BE2", "#A52A2A", "#DEB887", "#5F9EA0", "#7FFF00", "#D2691E", "#FF7F50", "#6495ED", "#FFF8DC", "#DC143C", "#00FFFF", "#00008B", "#008B8B", "#B8860B", "#A9A9A9", "#006400", "#A9A9A9", "#BDB76B", "#8B008B", "#556B2F", "#FF8C00", "#9932CC", "#8B0000", "#E9967A", "#8FBC8F", "#483D8B", "#2F4F4F", "#2F4F4F", "#00CED1", "#9400D3", "#FF1493", "#00BFFF", "#696969", "#696969", "#1E90FF", "#B22222", "#FFFAF0", "#228B22", "#FF00FF", "#DCDCDC", "#F8F8FF", "#FFD700", "#DAA520", "#808080", "#008000", "#ADFF2F", "#808080", "#F0FFF0", "#FF69B4", "#CD5C5C", "#4B0082", "#FFFFF0", "#F0E68C", "#E6E6FA", "#FFF0F5", "#7CFC00", "#FFFACD", "#ADD8E6", "#F08080", "#E0FFFF", "#FAFAD2", "#D3D3D3", "#90EE90", "#D3D3D3", "#FFB6C1", "#FFA07A", "#20B2AA", "#87CEFA", "#778899", "#778899", "#B0C4DE", "#FFFFE0", "#00FF00", "#32CD32", "#FAF0E6", "#FF00FF", "#800000", "#66CDAA", "#0000CD", "#BA55D3", "#9370DB", "#3CB371", "#7B68EE", "#00FA9A", "#48D1CC", "#C71585", "#191970", "#F5FFFA", "#FFE4E1", "#FFE4B5", "#FFDEAD", "#000080", "#FDF5E6", "#808000", "#6B8E23", "#FFA500", "#FF4500", "#DA70D6", "#EEE8AA", "#98FB98", "#AFEEEE", "#DB7093", "#FFEFD5", "#FFDAB9", "#CD853F", "#FFC0CB", "#DDA0DD", "#B0E0E6", "#800080", "#FF0000", "#BC8F8F", "#4169E1", "#8B4513", "#FA8072", "#F4A460", "#2E8B57", "#FFF5EE", "#A0522D", "#C0C0C0", "#87CEEB", "#6A5ACD", "#708090", "#708090", "#FFFAFA", "#00FF7F", "#4682B4", "#D2B48C", "#008080", "#D8BFD8", "#FF6347", "#40E0D0", "#EE82EE", "#F5DEB3", "#FFFFFF", "#F5F5F5", "#FFFF00", "#9ACD32"];
+var ColorList = ["#F0F8FF", "#FAEBD7", "#00FFFF", "#7FFFD4", "#F0FFFF",
+    "#F5F5DC", "#FFE4C4", "#000000", "#FFEBCD", "#0000FF", "#8A2BE2",
+    "#A52A2A", "#DEB887", "#5F9EA0", "#7FFF00", "#D2691E", "#FF7F50",
+    "#6495ED", "#FFF8DC", "#DC143C", "#00FFFF", "#00008B", "#008B8B",
+    "#B8860B", "#A9A9A9", "#006400", "#A9A9A9", "#BDB76B", "#8B008B",
+    "#556B2F", "#FF8C00", "#9932CC", "#8B0000", "#E9967A", "#8FBC8F",
+    "#483D8B", "#2F4F4F", "#2F4F4F", "#00CED1", "#9400D3", "#FF1493",
+    "#00BFFF", "#696969", "#696969", "#1E90FF", "#B22222", "#FFFAF0",
+    "#228B22", "#FF00FF", "#DCDCDC", "#F8F8FF", "#FFD700", "#DAA520",
+    "#808080", "#008000", "#ADFF2F", "#808080", "#F0FFF0", "#FF69B4",
+    "#CD5C5C", "#4B0082", "#FFFFF0", "#F0E68C", "#E6E6FA", "#FFF0F5",
+    "#7CFC00", "#FFFACD", "#ADD8E6", "#F08080", "#E0FFFF", "#FAFAD2",
+    "#D3D3D3", "#90EE90", "#D3D3D3", "#FFB6C1", "#FFA07A", "#20B2AA",
+    "#87CEFA", "#778899", "#778899", "#B0C4DE", "#FFFFE0", "#00FF00",
+    "#32CD32", "#FAF0E6", "#FF00FF", "#800000", "#66CDAA", "#0000CD",
+    "#BA55D3", "#9370DB", "#3CB371", "#7B68EE", "#00FA9A", "#48D1CC",
+    "#C71585", "#191970", "#F5FFFA", "#FFE4E1", "#FFE4B5", "#FFDEAD",
+    "#000080", "#FDF5E6", "#808000", "#6B8E23", "#FFA500", "#FF4500",
+    "#DA70D6", "#EEE8AA", "#98FB98", "#AFEEEE", "#DB7093", "#FFEFD5",
+    "#FFDAB9", "#CD853F", "#FFC0CB", "#DDA0DD", "#B0E0E6", "#800080",
+    "#FF0000", "#BC8F8F", "#4169E1", "#8B4513", "#FA8072", "#F4A460",
+    "#2E8B57", "#FFF5EE", "#A0522D", "#C0C0C0", "#87CEEB", "#6A5ACD",
+    "#708090", "#708090", "#FFFAFA", "#00FF7F", "#4682B4", "#D2B48C",
+    "#008080", "#D8BFD8", "#FF6347", "#40E0D0", "#EE82EE", "#F5DEB3",
+    "#FFFFFF", "#F5F5F5", "#FFFF00", "#9ACD32"
+];
 var LastStr = "";
+
+var keywords = ["in", "of", "if", "for", "while", "finally", "var", "new",
+    "function", "do", "return", "void", "else", "break", "catch",
+    "instanceof", "with", "throw", "case", "default", "try", "this",
+    "switch", "continue", "typeof", "delete", "let", "yield", "const",
+    "export", "super", "debugger", "as", "async", "await"
+];
+var literals = ["true", "false", "null", "undefined", "NaN", "Infinity"];
+var built_in = ["eval", "isFinite", "isNaN", "parseFloat", "parseInt",
+    "decodeURI", "decodeURIComponent", "encodeURI", "encodeURIComponent",
+    "escape", "unescape", "Object", "Function", "Boolean", "Error",
+    "EvalError", "InternalError", "RangeError", "ReferenceError",
+    "StopIteration", "SyntaxError", "TypeError", "URIError", "Number",
+    "Math", "Date", "String", "RegExp", "Array", "Float32Array",
+    "Float64Array", "Int16Array", "Int32Array", "Int8Array", "Uint16Array",
+    "Uint32Array", "Uint8Array", "Uint8ClampedArray", "ArrayBuffer",
+    "DataView", "JSON", "Intl", "arguments", "require", "module", "console",
+    "window", "document", "Symbol", "Set", "Map", "WeakSet", "WeakMap",
+    "Proxy", "Reflect", "Promise"
+];
+
+function jsText(InStr) {
+	var OutStr = InStr;
+	var CurrCol = 0;
+	
+	for (var x = 0; x < InStr.length; x++) {
+		for (var z = 0; z < keywords.length; z++) {
+			OutStr = InStr.replace(keywords[z], "[color=#FFCC66]" + keywords[z] + "[/color]")
+		}
+		for (var z = 0; z < literals.length; z++) {
+			OutStr = InStr.replace(literals[z], "[color=#CC3366]" + literals[z] + "[/color]")
+		}
+		for (var z = 0; z < built_in.length; z++) {
+			OutStr = InStr.replace(built_in[z], "[color=#33FFFF]" + built_in[z] + "[/color]")
+		}
+	}
+	return OutStr;
+}
 
 ///////COOKIE SHIT
 function setCookie(name, value, days) {
