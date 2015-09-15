f2d2042b680fba34f483bf01ed1294513ea92591
chrishansen69 <chrishansen69@users.noreply.github.com>
2015-09-14 23:20:35 -0300

    check those dubs

--- alive_toolset.js
+++ alive_toolset.js
@@ -10,3 +10,30 @@ function get_new_msg() {
     return false;
   }
 }
+
+function check_em() {
+  document.getElementById("frame_chatbox").contentWindow.
+  document.getElementById("chatbox").innerHTML = document.getElementById("frame_chatbox").contentWindow.
+  document.getElementById("chatbox").innerHTML.
+  replace(/\:11\]/g,
+          '\:11\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  replace(/\:22\]/g,
+          '\:22\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  replace(/\:33\]/g,
+          '\:33\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  replace(/\:44\]/g,
+          '\:44\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  replace(/\:55\]/g,
+          '\:55\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  replace(/\:66\]/g,
+          '\:66\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  replace(/\:77\]/g,
+          '\:77\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  replace(/\:88\]/g,
+          '\:88\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  replace(/\:99\]/g,
+          '\:99\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  replace(/\:00\]/g,
+          '\:00\]</span><span style=\"color\:red\"> CHECK \'EM</span><span class=\"date-and-time\">');
+  
+}
