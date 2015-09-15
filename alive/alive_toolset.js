958530ba7a12924e14b9fe731fb9a9a71c046a1f
chrishansen69 <chrishansen69@users.noreply.github.com>
2015-09-15 11:53:08 -0300

    Update alive_toolset.js

--- alive/alive_toolset.js
+++ alive/alive_toolset.js
@@ -11,7 +11,7 @@
 var css_string = "@import url(\'http://fonts.googleapis.com/css?family=Noto+Sans:400,700\');.box{width: 0px;height: 0px;position: relative;border: 0px solid #BBB;background: #EEE;font-family: \'Noto Sans\', sans-serif}.ribbon{width: 200px;background: #e43;position: absolute;top: 0px;left: -10px;text-align: center;line-height: 50px;letter-spacing: 1px;color: #ff0000;font-size: 18px}.ribbon span{width: 200px;background: #e43;position: absolute;top: 25px;left: -50px;text-align: center;line-height: 50px;letter-spacing: 1px;color: #f0f0f0;transform: rotate(-45deg);-webkit-transform: rotate(-45deg);font-size: 18px}.ribbon span::before{content: \"\";position:absolute;left:0px;top:100%;z-index:-1;border-left:3px solid #8F0808;border-right:3px solid transparent;border-bottom:3px solid transparent;border-top:3px solid #8F0808}.ribbon span::after{content:\"\";position:absolute;right:0px;top:100%;z-index:-1;border-left:3px solid transparent;border-right:3px solid #8F0808;border-bottom:3px solid transparent;border-top:3px solid #8F0808}";
 
 var preva = "";
-function get_new_msg() {
+function get_new_msg() { //do we have a new msg
   if (document.getElementById("frame_chatbox").contentWindow.document.getElementById("chatbox").innerHTML != preva) {
     preva = document.getElementById("frame_chatbox").contentWindow.document.getElementById("chatbox").innerHTML;
     return true;
@@ -21,14 +21,18 @@ function get_new_msg() {
   }
 }
 
-var is_at_cbox = false;
-var new_msgs = 0;
+var is_at_cbox = false; //have we scrolled to the chatbox
+var new_msgs = 0; //how many new msgs
 function get_new_msgs() {
-  if (!is_at_cbox) {
-    if (get_new_msg()) new_msgs++;
-  } else {
+  if (!is_at_cbox)
+    if (get_new_msg())
+      new_msgs++;
+  else
     new_msgs = 0;
-  }
+  if (new_msgs > 0) //make our nice loooking ribbon
+    box_div.innerHTML =  '<div class=\"box\" style=\"position: fixed;left: 1%;top: 2%;\"><div class=\"ribbon\"><span>' + new_msgs + ' new msgs</span></div></div>';
+  else //clear the div
+    box_div.innerHTML = '';
 }
 
 function check_em() {
@@ -69,11 +73,15 @@ function inject_css(css) {
     head.appendChild(gstyle);
 }
 
+var box_div; //init the box div so it can go anywhere
 window.onload = function() {
+  // inject our css
   inject_css(css_string);
+  
+  // make an empty div where the box will go
   var over_div = document.body;
-  var box_div = over_div.appendChild(document.createElement('div'));
-  box_div.innerHTML =  '<div class=\"box\" style=\"position: fixed;left: 1%;top: 2%;\"><div class=\"ribbon\"><span>NEW MESSAGE!</span></div></div>';
+  box_div = over_div.appendChild(document.createElement('div'));
+  box_div.innerHTML =  '';
   
   function getScrollTop() { //// http://stackoverflow.com/questions/6691558/how-do-i-make-a-div-follow-me-as-i-scroll-down-the-page
     if (typeof window.pageYOffset !== 'undefined' ) {
@@ -90,7 +98,10 @@ window.onload = function() {
     // IE in quirks mode
     return document.body.scrollTop;
   }
-
+  
+  // get the count of new msgs
+  setInterval(get_new_msgs, 500);
+  
   window.onscroll = function() {
     //make the box scroll with the screen
     var box = document.getElementById('box'),
