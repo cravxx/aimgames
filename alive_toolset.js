a129fc0aa684e0ba1b19a85622f63f0b037a79a5
rafa1231518 <chris331hansen@outlook.com>
2015-09-14 22:48:45 -0300

    Update alive_toolset.js

--- alive_toolset.js
+++ alive_toolset.js
@@ -1,7 +1,7 @@
 //test thing
 var preva = "";
 function get_new_msg() {
+  preva = document.getElementById("frame_chatbox").contentWindow.document.getElementById("chatbox").innerHTML;
   if (document.getElementById("frame_chatbox").contentWindow.document.getElementById("chatbox").innerHTML != preva) return true;
   else return false;
-  preva = document.getElementById("frame_chatbox").contentWindow.document.getElementById("chatbox").innerHTML;
 }
