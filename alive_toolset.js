b26b516376589ccb845ab1b3700774521be36ebd
chrishansen69 <chrishansen69@users.noreply.github.com>
2015-09-14 22:47:32 -0300

    i proceed to suck

--- alive_toolset.js
+++ alive_toolset.js
@@ -1,8 +1,7 @@
 //test thing
-var a = document.getElementById("frame_chatbox").contentWindow.document.getElementById("chatbox").innerHTML;
 var preva = "";
 function get_new_msg() {
-  if (a != preva) return true;
+  if (document.getElementById("frame_chatbox").contentWindow.document.getElementById("chatbox").innerHTML != preva) return true;
   else return false;
+  preva = document.getElementById("frame_chatbox").contentWindow.document.getElementById("chatbox").innerHTML;
 }
-preva = a;
