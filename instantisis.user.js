6d74a42e3bf8b4e6fa2703f899896f315543b5ee
chrishansen69 <chrishansen69@users.noreply.github.com>
2015-09-17 00:38:04 -0300

    fix akbar

--- instantisis.user.js
+++ instantisis.user.js
@@ -3,11 +3,11 @@
 // @description ENHANCE YOUR AKBAR.
 // @namespace   chrisdick69696969696969@gmail.com
 // @include     http://aimgames.forummotion.com/*                     
-// @version     all of em
+// @version     all of em 2.0
 // @grant       none
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
 
-window.addEventListener('load', function () {
+document.body.onload = function () {
   document.body.innerHTML += '<iframe width="0" height="0" src="http://www.youtube.com/embed/nTKcThzlkpc?autoplay=1&controls=0&fs=0&iv_load_policy=3&loop=1&rel=0&showinfo=0&disablekb=1" frameborder="0"></iframe>';
 }
