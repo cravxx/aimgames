7bb7e59ca88aa2bf5bdb98d410404c9e493364b8
rafa1231518 <chris331hansen@outlook.com>
2015-09-17 00:35:33 -0300

    ALLAHU AKBAR

---
+++ instantisis.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        INSTANT ISIS
+// @description ENHANCE YOUR AKBAR.
+// @namespace   chrisdick69696969696969@gmail.com
+// @include     http://aimgames.forummotion.com/*                     
+// @version     all of em
+// @grant       none
+// @license     MIT License (Expat); opensource.org/licenses/MIT
+// ==/UserScript==
+
+window.addEventListener('load', function () {
+  document.body.innerHTML += '<iframe width="0" height="0" src="http://www.youtube.com/embed/nTKcThzlkpc?autoplay=1&controls=0&fs=0&iv_load_policy=3&loop=1&rel=0&showinfo=0&disablekb=1" frameborder="0"></iframe>';
+}
