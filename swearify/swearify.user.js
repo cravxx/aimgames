8798097de5e51598a6db529e049d35381d262175
Daffeinatek <samsquanchhunter14@gmail.com>
2015-09-17 21:46:53 -0500

    greasy

--- swearify/swearify.user.js
+++ swearify/swearify.user.js
@@ -3,7 +3,7 @@
 // @description Adds a number of enhancements to your experience on AIM games.
 // @namespace   kaffeinition@gmail.com
 // @include     http://aimgames.forummotion.com/*                     
-// @version     2.9.73
+// @version     2.9.74
 // @grant       none
 // @icon        http://i.imgur.com/HlEs1B4.png
 // @license     MIT License (Expat); opensource.org/licenses/MIT
