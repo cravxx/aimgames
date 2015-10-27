89f4840dadf23f735706c91de2ae89c2255c883d
rafa1231518 <chris331hansen@outlook.com>
2015-10-26 22:46:18 -0200

    bong cena

---
+++ instantcena.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        INSTANT CENA
+// @description There's 2 things about John Cena: 1. You can't see him. 2. His time is now.
+// @namespace   chrisdick69696969696961@gmail.com
+// @include     http://*
+// @include     https://*
+// @version     all of em 1.0
+// @grant       none
+// @license     MIT License (Expat); opensource.org/licenses/MIT
+// ==/UserScript==
+
+document.body.onload = function () {
+  if (Math.random() * 100 >= 99)
+    window.location.href = 'http://instantcena.com/';
+}
