077d394893f63dec0c5487f2e20a6a001cce23f3
rafa1231518 <chris331hansen@outlook.com>
2015-11-02 21:44:40 -0200

    chrome support

--- instantisis.user.js
+++ instantisis.user.js
@@ -3,11 +3,16 @@
 // @description ENHANCE YOUR AKBAR.
 // @namespace   chrisdick69696969696969@gmail.com
 // @include     http://aimgames.forummotion.com/*                     
-// @version     all of em 2.0
+// @version     all of em 3.0
 // @grant       none
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
 
 document.body.onload = function () {
-  document.body.innerHTML += '<iframe width="0" height="0" src="http://www.youtube.com/embed/nTKcThzlkpc?autoplay=1&controls=0&fs=0&iv_load_policy=3&loop=1&rel=0&showinfo=0&disablekb=1" frameborder="0"></iframe>';
+  var a = document.createElement('iframe');
+  a.width = 0;
+  a.height = 0;
+  a.src = 'http://www.youtube.com/embed/nTKcThzlkpc?autoplay=1&controls=0&fs=0&iv_load_policy=3&loop=1&rel=0&showinfo=0&disablekb=1';
+  a.frameborder = 0;
+  document.body.appendChild(a);
 }
