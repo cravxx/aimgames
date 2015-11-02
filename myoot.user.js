43b0ae151467c7f9929c7ff14d503b882151b4c3
chrishansen69 <chrishansen69@users.noreply.github.com>
2015-11-02 21:06:13 -0200

    myoot lite

--- myoot.user.js
+++ myoot.user.js
@@ -3,31 +3,11 @@
 // @description Prevents any stray audio clips from automatically playing on AIM games.
 // @namespace   kaffeinition@gmail.com
 // @include     http://aimgames.forummotion.com/*
-// @version     1.3
+// @version     1.4
 // @icon        http://www.mediafire.com/convkey/c313/jnx13q9ha6j01w9zg.jpg
 // ==/UserScript==
 
-var audio_players = document.getElementsByTagName("audio");
-var loaded = false;
-
-var clips_stopped = 0;
-
-if(!loaded ){
-    for(var mute = 0; mute < audio_players.length; mute++){
-        audio_players[mute].muted = 1;
-        audio_players[mute].loop = false;
-        clips_stopped++;
-    }
+window.onload = function() {
+  for (var i in document.getElementsByTagName('audio'))
+    document.getElementsByTagName('audio')[i].autoplay = null
 }
-
-window.onload =          
-    function()   {
-        loaded = true;
-        console.log(clips_stopped + " clips stopped!");
-        if(clips_stopped == 1){
-            window.alert(clips_stopped + " clips stopped!\nThe URL for the clip is: " + audio_players[0].currentSrc);
-        }
-        if(clips_stopped > 1){
-            window.alert(clips_stopped + " clips stopped!\nToo many sources to list");
-        }
-};
