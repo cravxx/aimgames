59dd7ae02d454355e8a21d7e5a1386ff7694b8f6
chrishansen69 <chrishansen69@users.noreply.github.com>
2015-12-07 16:40:32 -0200

    eh fuck big chat anyways right guys

--- alive/chatboxplusplus.user.js
+++ alive/chatboxplusplus.user.js
@@ -3,7 +3,7 @@
 // @description Adds a number of 'universal' enhancements for the AIM Games chatbox. Warning: This script is still in active development and may contain bugs!
 // @namespace   the_thrasher@gmail.com
 // @include     http://aimgames.forummotion.com/
-// @version     1.32
+// @version     1.33
 // @grant       none
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
@@ -354,7 +354,6 @@ function go() {
 	  
 	  addRefreshButton();
   } else { //running in bchat
-	  window.alert('big chat is not supported by chatbox++, sorry');
 	  throw "big chat is not supported by chatbox++, sorry";
   }
 };
