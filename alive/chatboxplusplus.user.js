04ec724039c92365f542e15c60076ba40ffa7c95
rafa1231518 <chris331hansen@outlook.com>
2015-12-07 15:23:37 -0200

    increment version number

--- alive/chatboxplusplus.user.js
+++ alive/chatboxplusplus.user.js
@@ -3,7 +3,7 @@
 // @description Adds a number of 'universal' enhancements for the AIM Games chatbox. Warning: This script is still in active development and may contain bugs!
 // @namespace   the_thrasher@gmail.com
 // @include     http://aimgames.forummotion.com/*
-// @version     1.26
+// @version     1.27
 // @grant       none
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
