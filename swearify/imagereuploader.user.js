0f007def7d6b8d35ed75aabdf2a0a2a8e5e232ce
chrishansen69 <chrishansen69@users.noreply.github.com>
2015-12-09 21:58:16 -0200

    fix outerHTML usage

--- swearify/imagereuploader.user.js
+++ swearify/imagereuploader.user.js
@@ -2,9 +2,10 @@
 // @name            Swearify - Image Reuploader
 // @namespace       samsquanchhunter
 // @description     Special utility to quickly prepare images for insertion into Swearify.
-// @version         1.2
+// @version         1.3
 // @include         http://*
 // @include         https://*
+// @grant           none
 // ==/UserScript==
 
 ///portions from https://github.com/pinceladasdaweb/imgur-upload and LouCypher
@@ -12,10 +13,12 @@
 var body = document.body;
 body.addEventListener("contextmenu", initMenu, false);
 
-var menu = body.appendChild(document.createElement("menu"));
-menu.outerHTML = '<menu id="userscript-search-by-image" type="context">\
-                    <menuitem label="Resize and Upload"\
-                              icon="data:image/png;base64,\
+var menu = document.createElement("menu");
+menu.id = "userscript-search-by-image";
+menu.type = "context";
+var menuitem = document.createElement("menuitem");
+menuitem.label = "Resize and Upload";
+menuitem.icon = "data:image/png;base64,\
 iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAAlwSFlz\
 AAAK6wAACusBgosNWgAAABx0RVh0U29mdHdhcmUAQWRvYmUgRmlyZXdvcmtzIENTNXG14zYAAAEl\
 SURBVDiNY/z//z8DJYCRkIKsthv/kRX9Z2BgmFalARdiIcaGKZXqcH5O+01U+ay2G3MYGBiSiXUm\
@@ -23,8 +26,9 @@ mofnsBDSjEUTMkiBe2Eq1JnZ7TcZBHhZGNythBl0lLkZODmYGX7++sdw/sZnhl3H3zF8+voHwwsY\
 FkR5ijNICLMzTF31hOHnr38MHGxMDJlhMgwv3vxkWL7jJYpaJmzu0lTigWtmYGBg+PHrH8P0VU8Y\
 tJV5MNRiNYCfmxmuGQZ+/PrHwMmOqRyrAX///WfgYEOV4mBjwjAUpwHHL31iyA6XgRvCwcbEkBUm\
 w3DuxmcMtVgDkYONicHLVoTBSJOXgYONieHHz38Ml+98Ydh88DXDtx//CBtACmBiYGCYS4H+OYyU\
-5kasgUgKAADN8WLFzlj9rgAAAABJRU5ErkJggg=="></menuitem>\
-                  </menu>';
+5kasgUgKAADN8WLFzlj9rgAAAABJRU5ErkJggg==";
+menu.appendChild(menuitem);
+body.appendChild(menu);
 
 document.querySelector("#userscript-search-by-image menuitem")
   .addEventListener("click", uploadImage, false);
