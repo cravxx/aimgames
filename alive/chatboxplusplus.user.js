502ac92eed87b634c323a41c05ac17622842c372
rafa1231518 <chris331hansen@outlook.com>
2015-12-09 23:19:20 -0200

    fix the dubs fucking up alignment

--- alive/chatboxplusplus.user.js
+++ alive/chatboxplusplus.user.js
@@ -3,7 +3,7 @@
 // @description Adds a number of 'universal' enhancements for the AIM Games chatbox. Warning: This script is still in active development and may contain bugs!
 // @namespace   the_thrasher@gmail.com
 // @include     http://aimgames.forummotion.com/
-// @version     1.35
+// @version     1.36
 // @grant       none
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
@@ -81,9 +81,9 @@ function checkDubs() {
 	for (var i = 0; i < messages.length; i++) { //DON'T DO A FOREACH (VAR X IN Y) IN HTMLCOLLECTIONS IT WILL THROW AN ERROR
 		if (isDubs(msgTimestamp(messages[i]))) {
 			var dubsSpan = document.createElement('span');
-			dubsSpan.style = "color:red";
+			dubsSpan.style = "color: red; text-align: right; display: block; float: right;";
 			dubsSpan.innerHTML = ' CHECK \'EM';
-			messages[i].getElementsByClassName('date-and-time')[0].appendChild(dubsSpan);
+			messages[i].appendChild(dubsSpan);
 		}
 	}
 }
