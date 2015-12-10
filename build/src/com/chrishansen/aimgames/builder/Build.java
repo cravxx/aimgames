ade7bba36fcf001522567b1b09b3fdfd1f78118e
chrishansen69 <chris331hansen@outlook.com>
2015-12-10 13:01:17 -0200

    builder

---
+++ build/src/com/chrishansen/aimgames/builder/Build.java
@@ -0,0 +1,73 @@
+package com.chrishansen.aimgames.builder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Build {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		int linesToKeep = 0;
+		for (String line : Files.readAllLines(Paths.get("./instructions.txt"))) { //instructions should be in this file
+			if (line.startsWith("linesToKeep ")) {
+				linesToKeep = Integer.parseInt(line.substring(12));
+			} else if (!line.startsWith("#") && (!line.trim().equals(""))) { //if line isn't comment or empty
+				String lineFrom = line.substring(line.indexOf(" ") + 1).replace("\"", ""); //get only argument 2
+				System.out.println(lineFrom);
+				File f = new File("./" + lineFrom);
+				System.out.println("it exists? " + f.exists());
+				// read lines to keep
+				BufferedReader reader = new BufferedReader(new FileReader(f));
+				String keepLine = "";
+				for (int i = 0; i < linesToKeep; i++) {
+					keepLine = keepLine + reader.readLine() + "\r\n";
+				}
+				reader.close();
+				System.out.println(keepLine);
+
+				// compiler
+				compile(line);
+
+				// add kept lines
+				String lineTo = line.substring(0, line.indexOf(" ")).replace("\"", ""); //get only argument 1
+				System.out.println("lineto " + lineTo);
+				File fTo = new File("./" + lineTo);
+				FileInputStream fis = new FileInputStream(fTo);
+				BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+				String result = "";
+				String resultLine = "";
+				while( (resultLine = br.readLine()) != null){
+				 result = result + resultLine;
+				}
+				br.close();
+
+				result = keepLine + result;
+
+				fTo.delete();
+				FileOutputStream fos = new FileOutputStream(fTo);
+				fos.write(result.getBytes());
+				fos.flush();
+				fos.close();
+
+				linesToKeep = 0; //reset var (important!)
+		    } else {
+		    	System.out.println("line ignored");
+		    }
+		}
+	}
+
+	public static void compile(String instruction) throws IOException, InterruptedException {
+		// Run a java app in a separate system process
+		Process proc = Runtime.getRuntime().exec("java -jar compiler.jar --js_output_file=" + instruction);
+		proc.waitFor();
+		// Then retreive the process output
+		InputStream in = proc.getInputStream();
+		InputStream err = proc.getErrorStream();
+    }
+}
