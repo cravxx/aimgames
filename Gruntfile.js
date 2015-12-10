50928cd3126575bb2fba6544f6a5466874baeb7c
Daffeinatek <samsquanchhunter14@gmail.com>
2015-12-09 22:52:12 -0600

    minified

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,12 @@ module.exports = function(grunt) {
     uglify: {
       my_target: {
         files: {
-          'minjs/swearify.user.js': ['swearify/swearify.user.js']
+          'minjs/swearify.user.js': ['swearify/swearify.user.js'],
+          'minjs/imagereuploader.user.js': ['swearify/imagereuploader.user.js'],
+          'minjs/chameleon.user.js': ['chameleon/chameleon.user.js'],
+          'minjs/chameleon_random.user.js': ['chameleon/chameleon_random.user.js'],
+          'minjs/accountassist.user.js': ['other/accountassist.user.js'],
+          'minjs/ratebot.user.js': ['other/ratebot.user.js']
         }
       }
     }
