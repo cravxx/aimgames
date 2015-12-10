e44c2ac5ee1630c63f3c4645db59caf0aa048099
Daffeinatek <samsquanchhunter14@gmail.com>
2015-12-09 21:09:57 -0600

    ignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,22 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
+      options: {
+        ignores: ['node_modules/**/*.js', 'drone-ver/**/*.js']
+      },
       scripts: ['Gruntfile.js', '**/*.user.js', "**/*.js"]
-    }
+    },
+    watch: {
+     js: {
+       files: ["!node_modules/**", "!drone-ver/**", 'Gruntfile.js', '**/*.user.js', "**/*.js"],
+       tasks: ['jshint:scripts']
+     }
+   }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask('bleedTheFreak', ['jshint:scripts']);
 
 };
