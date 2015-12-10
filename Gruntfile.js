ec3649078440003889e04947134b31848172c19f
Daffeinatek <samsquanchhunter14@gmail.com>
2015-12-09 22:03:38 -0600

    uglifier

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,27 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      options: {
-        ignores: ['node_modules/**/*.js', 'drone-ver/**/*.js']
-      },
-      scripts: ['Gruntfile.js', '**/*.user.js', "**/*.js"]
+      scripts: ['Gruntfile.js', 'swearify/swearify.user.js']
     },
     watch: {
-     js: {
-       files: ["!node_modules/**", "!drone-ver/**", 'Gruntfile.js', '**/*.user.js', "**/*.js"],
-       tasks: ['jshint:scripts']
-     }
-   }
+      js: {
+        files: ['Gruntfile.js', 'swearify/swearify.user.js'],
+        tasks: ['jshint:scripts']
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'minjs/swearify.user.js': ['swearify/swearify.user.js']
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('bleedTheFreak', ['jshint:scripts']);
-
+  grunt.registerTask('makeRelease', ['uglify:my_target']);
 };
