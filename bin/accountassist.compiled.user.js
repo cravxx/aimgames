04d3b0d5fa399587326786db8e0afd4c7b805347
chrishansen69 <chris331hansen@outlook.com>
2015-12-10 15:20:45 -0200

    add latest binaries

---
+++ bin/accountassist.compiled.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Account Assist
+// @description bypass bullshit username checks
+// @namespace   samsquanchhunter14@gmail.com
+// @include     http://multiplayer.needformadness.com/registernew.pl
+// @include     https://multiplayer.needformadness.com/registernew.pl
+// @include     http://multiplayer.needformadness.com/edit.pl
+// @include     https://multiplayer.needformadness.com/edit.pl
+// @version     1.51
+// @grant       none
+// ==/UserScript==
+var p=document.createElement("script");p.innerHTML="function checkform(){return true;}function checkformL(){return true;}function checkformE(){var er = 0; return true;}function checkformN(){return true;}";document.head.appendChild(p);function go(){document.getElementById("main").style.visibility="visible";document.getElementById("gamebybg").style.visibility="visible";document.getElementById("gameby").style.visibility="visible";document.getElementById("menubg").style.visibility="visible";document.getElementById("menu").style.visibility="visible";document.getElementById("cmbg").style.visibility="visible";document.getElementById("cm").style.visibility="visible";document.getElementById("rest").style.visibility="visible";document.getElementById("user").maxLength=251;document.getElementById("pass").value="entershift";document.getElementById("cpass").value="entershift";document.getElementById("fname").value="snoop";document.getElementById("lname").value="dogg";document.getElementById("email").value="fuck"+Math.round(99999*Math.random())+"@shit.com";var a=document.createElement("button");a.textContent="Create random account";a.style="font-family: Verdana;font-style: italic;font-weight: bold;";a.onclick=function(){var a=Math.round(99999*Math.random());document.getElementById("user").value="chrishansen"+a;document.getElementById("pass").value="a";document.getElementById("cpass").value="a";document.getElementById("fname").value="a";document.getElementById("lname").value="a";document.getElementById("email").value="ac"+a+"@hansen.com";document.getElementsByName("reg")[0].submit()};document.getElementsByName("reg")[0].appendChild(a)}function init(){arguments.callee.done||(arguments.callee.done=!0,_timer&&clearInterval(_timer),go())}document.addEventListener&&document.addEventListener("DOMContentLoaded",init,!1);if(/WebKit/i.test(navigator.userAgent))var _timer=setInterval(function(){/loaded|complete/.test(document.readyState)&&init()},10);window.onload=init;
