ee3a7032acb73c44233958303c7a5572e1aa043d
Daffeinatek <samsquanchhunter14@gmail.com>
2015-12-14 19:48:02 -0600

    second psuh

--- swearify/imagereuploader.user.js
+++ swearify/imagereuploader.user.js
@@ -19,15 +19,7 @@ menu.type = "context";
 var menuitem = document.createElement("menuitem");
 menuitem.label = "Resize and Upload";
 /*jshint multistr: true */
-menuitem.icon = "data:image/png;base64,\
-iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAAlwSFlz\
-AAAK6wAACusBgosNWgAAABx0RVh0U29mdHdhcmUAQWRvYmUgRmlyZXdvcmtzIENTNXG14zYAAAEl\
-SURBVDiNY/z//z8DJYCRkIKsthv/kRX9Z2BgmFalARdiIcaGKZXqcH5O+01U+ay2G3MYGBiSiXUm\
-mofnsBDSjEUTMkiBe2Eq1JnZ7TcZBHhZGNythBl0lLkZODmYGX7++sdw/sZnhl3H3zF8+voHwwsY\
-FkR5ijNICLMzTF31hOHnr38MHGxMDJlhMgwv3vxkWL7jJYpaJmzu0lTigWtmYGBg+PHrH8P0VU8Y\
-tJV5MNRiNYCfmxmuGQZ+/PrHwMmOqRyrAX///WfgYEOV4mBjwjAUpwHHL31iyA6XgRvCwcbEkBUm\
-w3DuxmcMtVgDkYONicHLVoTBSJOXgYONieHHz38Ml+98Ydh88DXDtx//CBtACmBiYGCYS4H+OYyU\
-5kasgUgKAADN8WLFzlj9rgAAAABJRU5ErkJggg==";
+menuitem.icon = "http://i.imgur.com/F2wghzO.png";
 /*jshint multistr: false */
 menu.appendChild(menuitem);
 body.appendChild(menu);
