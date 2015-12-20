0c87577d972d47284f6e133e233e1daa37ca7da0
chrishansen69 <cadrekucra@gmx.com>
2015-12-20 15:44:55 -0200

    exit 1 crash if error

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ java -version
 ls -l
 cd build
 cd src/
-javac br/com/chrishansen/aimgames/builder/*.java
-jar cvfe build.jar br.com.chrishansen.aimgames.builder.Build br/com/chrishansen/aimgames/builder/*.class
+javac br/com/chrishansen/aimgames/builder/*.java || { exit 1; } # crashes if error
+jar cvfe build.jar br.com.chrishansen.aimgames.builder.Build br/com/chrishansen/aimgames/builder/*.class || { exit 1; } # crashes if error
 mv build.jar ../build.jar
 cd ..
-java -jar build.jar
+java -jar build.jar || { exit 1; } # crashes if error
