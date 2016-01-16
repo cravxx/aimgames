13b47b04aff39093d97ddea4fb22b80ba849e36a
--replace-all <samsquanchhunter14@gmail.com>
2016-01-16 01:42:42 -0600

    way better rainbow...will also allow for more variation

--- chameleon/chameleon_vampire.user.js
+++ chameleon/chameleon_vampire.user.js
@@ -1,125 +1,198 @@
 // ==UserScript==
-// @name        Chameleon Vampire
-// @namespace   Hide yo colors, hide yo styles
+// @name        Chameleon
+// @description Auto color formatting for the chatbox.
+// @namespace   how much grease
 // @include     http://aimgames.forummotion.com/*
-// @version     1.5
+// @version     1.5.3
 // @grant       none
+// @icon        http://i.imgur.com/g8MwvQd.png
+// @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
 
-function componentToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
+////// CODE FOR DEALING WITH OBJECTS
+function values(o) {
+  return Object.keys(o) .map(function (k) {
+    return o[k];
+  });
 }
+////// TOOLBOX
+function ayyliumLanguage(n)
+  {
+    var nybHexString = "0123456789ABCDEF";
+    return String(nybHexString.substr((n >> 4) & 0x0F,1)) + nybHexString.substr(n & 0x0F,1);
+  }
 
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
+function strangeAyylium(r,g,b)
+  {
+    return '#' + ayyliumLanguage(r) + ayyliumLanguage(g) + ayyliumLanguage(b);
+  }
 
 function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result ? {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
-  } : null;
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
 }
+//////
 
-function colourNameToHex(colour)
+//////COLOR
+var color_array = []; ///main array what we want gets moved into
+var color_style = [ "Normal", "Trippy", "Patriotic", "Warm" ];
+////
+//
+//
+var patriotic = [ "#ff0000", "#ffffff", "#0000ff" ];
+
+//needs work//var trippy = [ "#ff3300", "#0033cc", "#00cc00", "#ffcc00", "#cc00cc", "#6600ff", "#00ffff" ];
+var warm = [ "#ff3300", "#0033cc", "#00cc00", "#ffcc00", "#cc00cc", "#6600ff", "#00ffff" ];
+
+var normal=[];
+var frequency = .3;
+for (var i = 0; i < 32; ++i)
 {
-  var colours = {"aliceblue":"#f0f8ff","antiquewhite":"#faebd7","aqua":"#00ffff","aquamarine":"#7fffd4","azure":"#f0ffff",
-                 "beige":"#f5f5dc","bisque":"#ffe4c4","black":"#000000","blanchedalmond":"#ffebcd","blue":"#0000ff","blueviolet":"#8a2be2","brown":"#a52a2a","burlywood":"#deb887",
-                 "cadetblue":"#5f9ea0","chartreuse":"#7fff00","chocolate":"#d2691e","coral":"#ff7f50","cornflowerblue":"#6495ed","cornsilk":"#fff8dc","crimson":"#dc143c","cyan":"#00ffff",
-                 "darkblue":"#00008b","darkcyan":"#008b8b","darkgoldenrod":"#b8860b","darkgray":"#a9a9a9","darkgreen":"#006400","darkkhaki":"#bdb76b","darkmagenta":"#8b008b","darkolivegreen":"#556b2f",
-                 "darkorange":"#ff8c00","darkorchid":"#9932cc","darkred":"#8b0000","darksalmon":"#e9967a","darkseagreen":"#8fbc8f","darkslateblue":"#483d8b","darkslategray":"#2f4f4f","darkturquoise":"#00ced1",
-                 "darkviolet":"#9400d3","deeppink":"#ff1493","deepskyblue":"#00bfff","dimgray":"#696969","dodgerblue":"#1e90ff",
-                 "firebrick":"#b22222","floralwhite":"#fffaf0","forestgreen":"#228b22","fuchsia":"#ff00ff",
-                 "gainsboro":"#dcdcdc","ghostwhite":"#f8f8ff","gold":"#ffd700","goldenrod":"#daa520","gray":"#808080","green":"#008000","greenyellow":"#adff2f",
-                 "honeydew":"#f0fff0","hotpink":"#ff69b4",
-                 "indianred ":"#cd5c5c","indigo":"#4b0082","ivory":"#fffff0","khaki":"#f0e68c",
-                 "lavender":"#e6e6fa","lavenderblush":"#fff0f5","lawngreen":"#7cfc00","lemonchiffon":"#fffacd","lightblue":"#add8e6","lightcoral":"#f08080","lightcyan":"#e0ffff","lightgoldenrodyellow":"#fafad2",
-                 "lightgrey":"#d3d3d3","lightgreen":"#90ee90","lightpink":"#ffb6c1","lightsalmon":"#ffa07a","lightseagreen":"#20b2aa","lightskyblue":"#87cefa","lightslategray":"#778899","lightsteelblue":"#b0c4de",
-                 "lightyellow":"#ffffe0","lime":"#00ff00","limegreen":"#32cd32","linen":"#faf0e6",
-                 "magenta":"#ff00ff","maroon":"#800000","mediumaquamarine":"#66cdaa","mediumblue":"#0000cd","mediumorchid":"#ba55d3","mediumpurple":"#9370d8","mediumseagreen":"#3cb371","mediumslateblue":"#7b68ee",
-                 "mediumspringgreen":"#00fa9a","mediumturquoise":"#48d1cc","mediumvioletred":"#c71585","midnightblue":"#191970","mintcream":"#f5fffa","mistyrose":"#ffe4e1","moccasin":"#ffe4b5",
-                 "navajowhite":"#ffdead","navy":"#000080",
-                 "oldlace":"#fdf5e6","olive":"#808000","olivedrab":"#6b8e23","orange":"#ffa500","orangered":"#ff4500","orchid":"#da70d6",
-                 "palegoldenrod":"#eee8aa","palegreen":"#98fb98","paleturquoise":"#afeeee","palevioletred":"#d87093","papayawhip":"#ffefd5","peachpuff":"#ffdab9","peru":"#cd853f","pink":"#ffc0cb","plum":"#dda0dd","powderblue":"#b0e0e6","purple":"#800080",
-                 "red":"#ff0000","rosybrown":"#bc8f8f","royalblue":"#4169e1",
-                 "saddlebrown":"#8b4513","salmon":"#fa8072","sandybrown":"#f4a460","seagreen":"#2e8b57","seashell":"#fff5ee","sienna":"#a0522d","silver":"#c0c0c0","skyblue":"#87ceeb","slateblue":"#6a5acd","slategray":"#708090","snow":"#fffafa","springgreen":"#00ff7f","steelblue":"#4682b4",
-                 "tan":"#d2b48c","teal":"#008080","thistle":"#d8bfd8","tomato":"#ff6347","turquoise":"#40e0d0",
-                 "violet":"#ee82ee",
-                 "wheat":"#f5deb3","white":"#ffffff","whitesmoke":"#f5f5f5",
-                 "yellow":"#ffff00","yellowgreen":"#9acd32"};
-
-  if (typeof colours[colour.toLowerCase()] != 'undefined')
-    return colours[colour.toLowerCase()];
-  console.log(colours['red']);
-  return false;
+   var red   = Math.sin(frequency*i + 0) * 127 + 128;
+   var green = Math.sin(frequency*i + 2) * 127 + 128;
+   var blue  = Math.sin(frequency*i + 4) * 127 + 128;
+normal[i] = strangeAyylium(red,green,blue);
 }
+///////
 
+///////COOKIE SHIT
 function setCookie(name, value, days) {
+  var expires = '';
   if (days) {
     var date = new Date();
-    var expires = '';
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
     expires = '; expires=' + date.toGMTString();
-  } 
-  else var expires = '';
+  }
+  else expires = '';
   document.cookie = name + '=' + value + expires + '; path=/';
 }
 
-var spec_code = [
-  '/exit', '/away', '/abs', '/me'
-];
-
-$(document).on("keydown", function(e){
-  if (e.which === 13) { /// 13 is enter
-    if ($('.msg').length > 0) {
-      var msg = $(".msg");
-      var more = msg[msg.length - 1].children;
-      var layer = more[0].style.color;
-      //console.log(layer);
-      var old_msg = document.getElementById('message').value;  
-      if(old_msg){
-        var exit_code = old_msg.indexOf(spec_code[0]);
-        var away_code = old_msg.indexOf(spec_code[1]);
-        var abs_code = old_msg.indexOf(spec_code[2]);
-        var me_code = old_msg.indexOf(spec_code[3]);
-
-        if (exit_code == -1 && away_code == -1 && abs_code == -1 && me_code == -1) {
-          if(more[0].getElementsByTagName("strong").length > 0){        
-            old_msg = '[b]' + old_msg + '[/b]'; 
-          }
-          if(more[0].getElementsByTagName("i").length > 0 || more[0].getElementsByTagName("em").length > 0){       
-            old_msg = '[i]' + old_msg + '[/i]'; 
-          }
-          if(more[0].getElementsByTagName("u").length > 0){        
-            old_msg = '[u]' + old_msg + '[/u]'; 
-          }
-          if(more[0].getElementsByTagName("strike").length > 0){
-            old_msg = '[strike]' + old_msg + '[/strike]';
-          }          
-          document.getElementById('message').value = old_msg;          
-        }  
-      }
-      var test = layer.indexOf("("); 
-      if(test != -1){
-        var strCol = layer.substr(4, layer.length - 5).split(",");    
-        var red = parseInt(strCol[0]);
-        var green = parseInt(strCol[1]);
-        var blue = parseInt(strCol[2]);        
-        var hex = rgbToHex(red, green, blue);
-        document.getElementById('scolor') .value = hex;
-        document.getElementById('divcolor-preview') .style.cssText = 'background-color: rgb(' + red + ',' + green + ',' + blue + ');';
-        document.getElementById('message') .style.color = hex;
-        setCookie('CB_color', hex, 1);
-      }else{
-        var setCol = colourNameToHex(layer);
-        document.getElementById('scolor') .value = setCol;
-        document.getElementById('divcolor-preview') .style.cssText = 'background-color: rgb(' + hexToRgb(setCol).r + ',' + hexToRgb(setCol).g + ',' + hexToRgb(setCol).b + ');';
-        document.getElementById('message') .style.color = setCol;
-        setCookie('CB_color', setCol, 1);
-      }
-    }
+function getCookie(c_name) {
+  var name = c_name + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1);
+    if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
   }
-}); 
+  return '';
+}
+///////
+
+function returnOption(text){
+	var option = document.createElement("option");
+	option.text = text;
+	option.style.dir = "rtl";
+	option.style.paddingRight = "4px";
+	return option;
+}
+
+function createSelectBox(){
+	var new_td = document.createElement("td");
+	new_td.id = "Cha";
+
+  	document.getElementById("chatbox_messenger_form").getElementsByTagName("tr")[0].insertBefore(
+		new_td, 
+		document.getElementById("chatbox_messenger_form").getElementsByTagName("tr")[0].childNodes[0]
+  	);
+
+	var gen_location = document.getElementById("Cha");
+        select = document.createElement('select');
+        select.id = 'selectCha';        
+        gen_location.appendChild(select);	
+
+	var sel_location = document.getElementById('selectCha');	
+	
+	sel_location.addEventListener('change', function() {whatdo(this);}, false);
+	for(var num_entries = 0; num_entries < color_style.length; num_entries++){
+	   sel_location.appendChild(returnOption(color_style[num_entries]));
+	}
+}
+
+var counter = 0;
+
+function whatdo(wew){    
+    var dunwan = [];
+    color_array = []; //clear it
+   
+   if(wew.value == "Normal"){
+    dunwan = normal;
+   }else if(wew.value == "Patriotic"){
+    dunwan = patriotic;
+   }else if(wew.value == "Warm"){
+    dunwan = warm;
+   }
+   
+   for (var all = 0; all < dunwan.length; all++) {
+        color_array[all] = dunwan[all];
+        console.log("kek");
+   }
+   counter = 0;
+}
+
+window.addEventListener('load', function() {
+   createSelectBox();
+   for (var all = 0; all < normal.length; all++) { ////load normal at first
+        color_array[all] = normal[all];
+        console.log("only once pls");
+   }
+   
+if (document.getElementById('frame_chatbox') !== null || document.getElementById('message') !== null) {
+  if (window.location.pathname.length <= 1) {
+    if (getCookie('chameleon_counter') === 0) {
+      setCookie('CB_color', color_array[counter], 1);
+    } else{
+      counter = getCookie('chameleon_counter');
+    }
+    document.getElementById('frame_chatbox') .contentWindow.document.getElementById('message') .addEventListener('keydown', function (event) {
+      var code = (event.keyCode) ? event.keyCode : event.which;
+      if (code == 13) {
+        counter++;
+        if (counter == color_array.length) {
+          counter = 0;
+        }
+      }
+    }, false);
+    document.getElementById('frame_chatbox') .contentWindow.document.getElementById('message') .addEventListener('keyup', function (event) {
+      var code = (event.keyCode) ? event.keyCode : event.which;
+      if (code == 13) {
+        setCookie('chameleon_counter', counter, 1);
+        setCookie('CB_color', color_array[counter], 1);
+        document.getElementById('frame_chatbox') .contentWindow.document.getElementById('scolor') .value = color_array[counter];
+        document.getElementById('frame_chatbox') .contentWindow.document.getElementById('divcolor-preview') .style.cssText = 'background-color: rgb(' + hexToRgb(color_array[counter]).r + ',' + hexToRgb(color_array[counter]).g + ',' + hexToRgb(color_array[counter]).b + ');';
+        document.getElementById('frame_chatbox') .contentWindow.document.getElementById('message') .style.color = color_array[counter];
+      }
+    }, false);
+  } else {
+    if (getCookie('chameleon_counter') === 0) {
+      setCookie('CB_color', color_array[counter], 1);
+    } else{
+      counter = getCookie('chameleon_counter');
+    }
+    document.getElementById('message') .addEventListener('keydown', function (event) {
+      var code = (event.keyCode) ? event.keyCode : event.which;
+      if (code == 13) {
+        counter++;
+        if (counter == color_array.length) {
+          counter = 0;
+        }
+      }
+    }, false);
+    document.getElementById('message') .addEventListener('keyup', function (event) {
+      var code = (event.keyCode) ? event.keyCode : event.which;
+      if (code == 13) {
+        setCookie('chameleon_counter', counter, 1);
+        setCookie('CB_color', color_array[counter], 1);
+        console.log(color_array[counter]);
+        document.getElementById('scolor') .value = color_array[counter];
+        document.getElementById('divcolor-preview') .style.cssText = 'background-color: rgb(' + hexToRgb(color_array[counter]).r + ',' + hexToRgb(color_array[counter]).g + ',' + hexToRgb(color_array[counter]).b + ');';
+        document.getElementById('message') .style.color = color_array[counter];
+      }
+    }, false);
+  }
+}  
+}, false);
