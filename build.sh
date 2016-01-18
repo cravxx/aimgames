09ace755bd021e4926dc7d1f7f3653b7b71b8993
chrishansen69 <cadrekucra@notmailinator.com>
2016-01-17 23:03:30 -0200

    fix bash again

--- build.sh
+++ build.sh
@@ -15,11 +15,11 @@ cd build
 cd src/
 
 #compile everything
-javac br/com/chrishansen/aimgames/builder/*.java
+javac br/com/chrishansen/aimgames/builder/*.java || exit 1
 #pack AddUserscriptLines
-jar cvfe AddUserscriptLines.jar br.com.chrishansen.aimgames.builder.AddUserscriptLines br/com/chrishansen/aimgames/builder/AddUserscriptLines.class
+jar cvfe AddUserscriptLines.jar br.com.chrishansen.aimgames.builder.AddUserscriptLines br/com/chrishansen/aimgames/builder/AddUserscriptLines.class || exit 1
 #pack IncrementBuildNumber
-jar cvfe IncrementBuildNumber.jar br.com.chrishansen.aimgames.builder.IncrementBuildNumber br/com/chrishansen/aimgames/builder/IncrementBuildNumber.class
+jar cvfe IncrementBuildNumber.jar br.com.chrishansen.aimgames.builder.IncrementBuildNumber br/com/chrishansen/aimgames/builder/IncrementBuildNumber.class || exit 1
 
 mv AddUserscriptLines.jar ../AddUserscriptLines.jar
 mv IncrementBuildNumber.jar ../IncrementBuildNumber.jar
@@ -29,8 +29,8 @@ cd ..
 
 #Argument 1 is the file that will be output, argument 2 is the file that will be input, argument 3 is the number of lines to be kept from the start of the file.
 #../ means "go back one folder"
-sh compile ../bin/swearify.compiled.user.js "../swearify/swearify.user.js" 15
-sh increment
+sh compile.sh ../bin/swearify.compiled.user.js "../swearify/swearify.user.js" 15
+sh increment.sh
 
 #return to home
 cd ..
