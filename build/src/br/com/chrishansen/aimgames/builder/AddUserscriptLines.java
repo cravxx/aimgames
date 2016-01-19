d4cddef3cdb6d82d9c9fde5a5bb10baa46876d38
chrishansen69 <cadrekucra@notmailinator.com>
2016-01-19 12:35:36 -0200

    remove unnecessary imports

--- build/src/br/com/chrishansen/aimgames/builder/AddUserscriptLines.java
+++ build/src/br/com/chrishansen/aimgames/builder/AddUserscriptLines.java
@@ -7,9 +7,6 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class AddUserscriptLines {
     public static void main(final String[] args) throws IOException, InterruptedException {
