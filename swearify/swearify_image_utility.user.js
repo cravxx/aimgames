b2e560ff9721ed9ea9280e5c90e4fbcc9243b90d
Daffeinatek <samsquanchhunter14@gmail.com>
2016-03-09 22:41:42 -0600

    yeah this doesnt work but it's closer to being right

--- swearify/swearify_image_utility.user.js
+++ swearify/swearify_image_utility.user.js
@@ -1,29 +1,26 @@
 // ==UserScript==
 // @name        Swearify Image Utility
 // @namespace   samsquanchhunter
-// @include     http://*
-// @include     https://*
-// @version     alpha.1.2
+// @include     *
+// @version     alpha.1.3
 // @icon        http://i.imgur.com/MnWNRBL.png
 // @grant       none
 // ==/UserScript==
+
 /**
  * a rewrite of the Image Reuploader userscript
  *
  */
+
 /**
  * some utility functions
  */
-Element.prototype.remove = function()
-{
+Element.prototype.remove = function() {
     this.parentElement.removeChild(this);
 }
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function()
-{
-    for (var i = this.length - 1; i >= 0; i--)
-    {
-        if (this[i] && this[i].parentElement)
-        {
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for (var i = this.length - 1; i >= 0; i--) {
+        if (this[i] && this[i].parentElement) {
             this[i].parentElement.removeChild(this[i]);
         }
     }
@@ -32,18 +29,14 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove = function()
 /**
  * create the context item
  */
-function createStuff()
-{
-    document.body.addEventListener("contextmenu", onRightClick, false);
+function createStuff() {
+    document.addEventListener("contextmenu", onRightClick, false);
 
-    if (document.getElementsByTagName("menu").length === 0)
-    {
+    if (document.getElementsByTagName("menu").length === 0) {
         var menu = document.createElement("menu");
         menu.id = "userscript-grease";
         menu.type = "context";
-    }
-    else
-    {
+    } else {
         menu = document.getElementsByTagName("menu")[0];
     }
     var menuitem = document.createElement("menuitem");
@@ -54,34 +47,41 @@ function createStuff()
     document.body.appendChild(menu);
 
     document.querySelector("#userscript-grease #menu_imgre")
-        .addEventListener("click", createCanvas, false);
+        .addEventListener("click", checkImageOrigin, false);
 }
 
-function onRightClick(e)
-{
+function onRightClick(e) {
     // Executed when user right click on web page body
     // aEvent.target is the element you right click on
     document.body.setAttribute("contextmenu", "userscript-grease");
     var node = e.target;
     link = e.target.getAttribute("imageURL");
     var item = document.querySelector("#userscript-grease #menu_imgre");
-    if (node.localName == "img")
-    {
+    if (node.localName == "img") {
         item.disabled = false;
         item.setAttribute("imageURL", node.src);
-    }
-    else
-    {
+    } else {
         item.disabled = true;
     }
 }
 
+/**
+ * if the image is from the same origin, we can place it into a canvas
+ * if not, we just upload it to imgur
+ */
+function checkImageOrigin(){
+    var pageOrigin = window.location.origin;
+    var imageOrigin = document.getElementById('menu_imgre').getAttribute('imageurl');
+    
+    console.log(pageOrigin + " " + imageOrigin);
+    
+    createCanvas();
+}
 
 /**
  * part of a lifesaving library https://github.com/viliusle/Hermite-resize
  */
-function resample_hermite(canvas, W, H, W2, H2)
-{
+function resample_hermite(canvas, W, H, W2, H2) {
     var time1 = Date.now();
     W2 = Math.round(W2);
     H2 = Math.round(H2);
@@ -94,31 +94,25 @@ function resample_hermite(canvas, W, H, W2, H2)
     var ratio_w_half = Math.ceil(ratio_w / 2);
     var ratio_h_half = Math.ceil(ratio_h / 2);
 
-    for (var j = 0; j < H2; j++)
-    {
-        for (var i = 0; i < W2; i++)
-        {
+    for (var j = 0; j < H2; j++) {
+        for (var i = 0; i < W2; i++) {
             var x2 = (i + j * W2) * 4;
             var weight = 0;
             var weights = 0;
             var weights_alpha = 0;
             var gx_r = gx_g = gx_b = gx_a = 0;
             var center_y = (j + 0.5) * ratio_h;
-            for (var yy = Math.floor(j * ratio_h); yy < (j + 1) * ratio_h; yy++)
-            {
+            for (var yy = Math.floor(j * ratio_h); yy < (j + 1) * ratio_h; yy++) {
                 var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
                 var center_x = (i + 0.5) * ratio_w;
                 var w0 = dy * dy //pre-calc part of w
-                for (var xx = Math.floor(i * ratio_w); xx < (i + 1) * ratio_w; xx++)
-                {
+                for (var xx = Math.floor(i * ratio_w); xx < (i + 1) * ratio_w; xx++) {
                     var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
                     var w = Math.sqrt(w0 + dx * dx);
-                    if (w >= -1 && w <= 1)
-                    {
+                    if (w >= -1 && w <= 1) {
                         //hermite filter
                         weight = 2 * w * w * w - 3 * w * w + 1;
-                        if (weight > 0)
-                        {
+                        if (weight > 0) {
                             dx = 4 * (xx + yy * W);
                             //alpha
                             gx_a += weight * data[dx + 3];
@@ -150,8 +144,7 @@ function resample_hermite(canvas, W, H, W2, H2)
 /**
  * create a canvas then call the function to resize the image into the canvas
  */
-function createCanvas()
-{
+function createCanvas() {
     var bodyCanvas = document.createElement('canvas');
     bodyCanvas.id = 'canvasImg';
     bodyCanvas.style = 'display:none;';
@@ -163,15 +156,13 @@ function createCanvas()
  * gets the canvas created in the last function and places the resized image into it
  * after waiting a timeout to make sure the canvas is filled, the uploadtoimgur function is called
  */
-function resizeIntoCanvas()
-{
+function resizeIntoCanvas() {
     var canvas = document.getElementById('canvasImg');
     var ctx = canvas.getContext("2d");
 
-    var img = new Image();
+    var img = new Image();    
     img.crossOrigin = "Anonymous"; //cors support
-    img.onload = function()
-    {
+    img.onload = function() {
         var W = img.width;
         var H = img.height;
         canvas.width = W;
@@ -181,47 +172,87 @@ function resizeIntoCanvas()
         //resize by ratio
         var prefH = 50;
         var heightR = prefH / H;
-        var ratio = 0.43895525; //from 0 to 1
+        //var ratio = 0.43895525; //from 0 to 1
         resample_hermite(canvas, W, H, Math.round(W * heightR), prefH);
 
         //resize manually
         //resample_hermite(canvas, W, H, 439, 222);
     }
-    img.src = document.getElementById('menu_imgre').getAttribute('imageurl');
-    setTimeout(function()
-    {
-        uploadtoImgur(canvas);
-        document.getElementById('canvasImg').remove();
-    }, 1000);
+    var str = document.getElementById('menu_imgre').getAttribute('imageurl');    
+    console.log(str);
+    
+    /* before the .src is set, we'll add a event listener */
+    
+    var booleanError = false;
+    
+    img.addEventListener('error', function (e) {
+        e.preventDefault(); // Prevent error from getting thrown
+        console.log('errored');
+        booleanError = true;
+    });
+    
+    img.src = str;
+    
+    if(booleanError){
+        /* errors, default to imgur direct */
+        uploadImagetoImgur(document.getElementById('menu_imgre').getAttribute('imageurl'));
+    } else {
+        /* no errors */
+        setTimeout(function() {
+            uploadCanvastoImgur(canvas);        
+            document.getElementById('canvasImg').remove();
+        }, 1000);    
+    }              
 }
 
 /**
  * this uploads the canvas passed to it
  */
-function uploadtoImgur(canvas)
-{
-    console.log('attempting to upload');
+function uploadImagetoImgur(dataIn) {
+    console.log('attempting to upload');    
+    /* */
 
     var fd = new FormData();
-    fd.append("image", canvas.toDataURL().split(',')[1]); // Append the file
-    fd.append("type", 'base64');
+    fd.append("image", dataIn); // Append the file
+    
     var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
     xhr.open("POST", "https://api.imgur.com/3/image.json"); // Boooom!
-    xhr.onload = function()
-        {
-            var link = JSON.parse(xhr.responseText).data.link;
-            alert(link);
-        }
-        // Ok, I don't handle the errors. An exercice for the reader.
+    xhr.onload = function() {
+        var link = JSON.parse(xhr.responseText).data.link;
+        alert(link);
+    }
+    xhr.onerror = function() { alert('error'); };
     xhr.setRequestHeader('Authorization', 'Client-ID d8b88dd7493540b');
-    /* And now, we send the formdata */
+    xhr.send(fd);
+}
+
+/**
+ * this uploads the canvas passed to it
+ */
+function uploadCanvastoImgur(dataIn) {
+    console.log('attempting to upload');    
+    /* */
+
+    var fd = new FormData();
+    fd.append("image", dataIn.toDataURL().split(',')[1]); // Append the file
+    fd.append("type", 'base64');
+    
+    var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+    xhr.open("POST", "https://api.imgur.com/3/image.json"); // Boooom!
+    xhr.onload = function() {
+        var link = JSON.parse(xhr.responseText).data.link;
+        alert(link);
+    }
+    xhr.onerror = function() { alert('error'); };
+    xhr.setRequestHeader('Authorization', 'Client-ID d8b88dd7493540b');
     xhr.send(fd);
 }
 
 /**
  * uhh
  */
-window.onload = function()
-{
-    createStuff();
-};
+if (document.addEventListener)
+  document.addEventListener("DOMContentLoaded", createStuff(), false);
+
+
+
