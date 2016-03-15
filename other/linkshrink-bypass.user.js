46dfdf4ae37a5da0d29c643b6441b1f13c9be309
chrishansen69 <chrishansen69@users.noreply.github.com>
2016-03-15 18:39:07 -0300

    Update linkshrink-bypass.user.js

--- other/linkshrink-bypass.user.js
+++ other/linkshrink-bypass.user.js
@@ -4,21 +4,29 @@
 // @namespace   by-mc-passer@gmail.com
 // @match       http://linkshrink.net/*
 // @match       https://linkshrink.net/*
-// @version     1.4
+// @version     1.6
 // @grant       none
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
 
-// get and delete bad html
-var a = $.find('script[type="text/javascript"]');
+// get and delete bad html (this won't work since it's assigned very quickly)
+/*var a = $.find('script[type="text/javascript"]');
 
 for (var i in a) {
 	if (a[i].textContent.indexOf('shipthankrecognizing') != -1) { //content check
 		a[i].parentNode.removeChild(a[i]); //make node delete itself
 	}
-}
+}*/
 
 // auto show skip ad and click (setting location.href doesn't work since referer is required)
 $("#skip").show();
 $("#pause").hide();
-$('#skip').find('.bt')[0].click();
+// using click() invokes the eventlistener, so we have to outsmart it
+//$('#skip').find('.bt')[0].click();
+
+function forceGotoLink(href) {
+	//var href = $('a').attr('href');
+	$('<form>').attr({action: href, method: 'GET'}).appendTo($('body')).submit();
+}
+
+forceGotoLink($('#skip').find('.bt').attr('href'));
