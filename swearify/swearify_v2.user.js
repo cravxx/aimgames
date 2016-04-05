8a24b91549ad2e633b8095c26fb07def29a4dd3c
Daffeinatek <samsquanchhunter14@gmail.com>
2016-04-05 16:22:35 -0500

    increment

--- swearify/swearify_v2.user.js
+++ swearify/swearify_v2.user.js
@@ -8,7 +8,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
 // @require     https://cdn.rawgit.com/HulaSamsquanch/aimgames/master/swearify/jquery.caret.1.02.min.js
 // @require     https://cdn.rawgit.com/HulaSamsquanch/aimgames/master/swearify/textUtils.js
-// @version     beta.3.4
+// @version     beta.3.5
 // @icon        http://i.imgur.com/MnWNRBL.png
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // @homepage    https://github.com/HulaSamsquanch/aimgames
