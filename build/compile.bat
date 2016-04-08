fe6f0458574cc4213e99c7ebeefd77daa57aea16
HulaSamsquanch <samsquanchhunter14@gmail.com>
2016-04-08 18:31:05 -0500

    ecma

--- build/compile.bat
+++ build/compile.bat
@@ -1,3 +1,3 @@
-java -jar GoogleClosureCompiler.jar --js_output_file=%1 %2
+java -jar GoogleClosureCompiler.jar --language_in=ECMASCRIPT6_STRICT --js_output_file=%1 %2
 java -jar AddUserscriptLines.jar %1 %2 %3
 java -jar BeautifyScript.jar %2
