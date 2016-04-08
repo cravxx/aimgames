5b42801f1da6d2d0986f822412c79b8869e09386
HulaSamsquanch <samsquanchhunter14@gmail.com>
2016-04-08 18:38:44 -0500

    uh that didn't...

--- build/compile.sh
+++ build/compile.sh
@@ -1,2 +1,2 @@
-java -jar GoogleClosureCompiler.jar --language_in=ECMASCRIPT6_STRICT --js_output_file=$1 $2
+java -jar GoogleClosureCompiler.jar --language_in=ECMASCRIPT6_STRICT --language_in=ES5_STRICT --js_output_file=$1 $2
 java -jar AddUserscriptLines.jar $1 $2 $3
