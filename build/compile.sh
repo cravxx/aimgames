f508bffeadbbc25bd14cf63b139cab455e523d50
HulaSamsquanch <samsquanchhunter14@gmail.com>
2016-04-08 18:31:05 -0500

    ecma

--- build/compile.sh
+++ build/compile.sh
@@ -1,2 +1,2 @@
-java -jar GoogleClosureCompiler.jar --js_output_file=$1 $2
+java -jar GoogleClosureCompiler.jar --language_in=ECMASCRIPT6_STRICT --js_output_file=$1 $2
 java -jar AddUserscriptLines.jar $1 $2 $3
