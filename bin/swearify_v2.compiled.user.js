b5b58b37f12f68f4281918c9681b25412853ca53
Appveyor Automated Build <bbbbbbenes@outlook.com.br>
2016-04-08 23:37:14 +0000

    Automated Appveyor build at 2016-04-08, 11:37 PM [skip ci]

---
+++ bin/swearify_v2.compiled.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Swearify 2.0
+// @description Adds a number of enhancements to your experience on AIM Games.
+// @namespace   samsquanchhunter14@gmail.com
+// @include     http://aimgames.forummotion.com/*
+// @include     https://aimgames.forummotion.com/*
+// @require     https://cdn.rawgit.com/js-cookie/js-cookie/master/src/js.cookie.js
+// @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js
+// @require     https://cdn.rawgit.com/HulaSamsquanch/aimgames/master/swearify/jquery.caret.1.02.min.js
+// @require     https://cdn.rawgit.com/HulaSamsquanch/aimgames/master/swearify/textUtils.js
+// @version     beta.3.6
+// @icon        http://i.imgur.com/MnWNRBL.png
+// @license     MIT License (Expat); opensource.org/licenses/MIT
+// @homepage    https://github.com/HulaSamsquanch/aimgames
+// @supportURL  https://github.com/HulaSamsquanch/aimgames/issues
+// @grant       none
+// ==/UserScript==
