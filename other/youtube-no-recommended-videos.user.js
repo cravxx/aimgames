2e4396a39292d3bc7c730acce3c3695785a4f934
rafa1231518 <chris331hansen@outlook.com>
2016-04-23 20:10:26 -0300

    Update youtube-no-recommended-videos.user.js

--- other/youtube-no-recommended-videos.user.js
+++ other/youtube-no-recommended-videos.user.js
@@ -4,7 +4,7 @@
 // @namespace   jojohansen420@gmail.com
 // @include     https://www.youtube.com/watch?v=*
 // @include     http://www.youtube.com/watch?v=*
-// @version     1.0
+// @version     1.1
 // @grant       none
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
@@ -13,7 +13,7 @@
 var vCounts = document.querySelectorAll('.related-list-item-compact-video > .content-wrapper > .content-link > .view-count.stat');
 
 for (var i of vCounts) {
-  if (i.textContent.startsWith('Recommended') || i.textContent.startsWith('Recomendado') {
+  if (i.textContent.startsWith('Recommended') || i.textContent.startsWith('Recomendado')) {
     i.parentNode.parentNode.parentNode.parentNode.removeChild(i.parentNode.parentNode.parentNode); //delete the video
   }
 }
