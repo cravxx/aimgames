3defa2737e5f4bf76ca175248c010cd547fa07ca
rafa1231518 <chris331hansen@outlook.com>
2016-05-14 16:32:50 -0300

    Create wikia-bypass.user.js

---
+++ other/wikia-bypass.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Direct external links for Wikia
+// @description Bypass adds when clicking external links in Wikia pages
+// @namespace   hansen-i-nate24214@gmail.com
+// @match       http://*.wikia.com/*
+// @match       https://*.wikia.com/*
+// @version     1.0
+// @grant       none
+// @license     MIT License (Expat); opensource.org/licenses/MIT
+// ==/UserScript==
+"use strict";
+
+// remove click listeners for this external links
+$('.external.text.exitstitial').off("click");
