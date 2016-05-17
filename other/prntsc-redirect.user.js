521b2cc38f011aca889f162894c8b058e9c97c29
rafa1231518 <chris331hansen@outlook.com>
2016-05-16 22:51:15 -0300

    Create prntsc-redirect.user.js

---
+++ other/prntsc-redirect.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        printscr redirect
+// @description redirects you to image on http://prnt.sc
+// @namespace   trashphotographer@gmail.com
+// @include     https://prnt.sc/*
+// @include     http://prnt.sc/*
+// @version     1.0
+// @grant       none
+// @license     MIT License (Expat); opensource.org/licenses/MIT
+// ==/UserScript==
+document.location.href=document.getElementById('screenshot-image').src.substr(31);
