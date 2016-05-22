57afd4895f4fd4dcc3051c33256bb1de305f6535
rafa1231518 <chris331hansen@outlook.com>
2016-05-22 11:37:13 -0300

    disable beautifier in compile.bat

--- build/compile.bat
+++ build/compile.bat
@@ -1,3 +1,3 @@
 java -jar GoogleClosureCompiler.jar --language_in=ECMASCRIPT6_STRICT --language_out=ES5_STRICT --compilation_level=ADVANCED_OPTIMIZATIONS --js_output_file=%1 %2
 java -jar AddUserscriptLines.jar %1 %2 %3
-java -jar BeautifyScript.jar %2
+@REM java -jar BeautifyScript.jar %2
