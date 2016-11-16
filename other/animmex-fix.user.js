86a6359f1bde78a3dda3ec83ed65c420c9c088e3
rafa1231518 <chris331hansen@outlook.com>
2016-11-15 23:28:59 -0200

    animmex fix now blocks popups + uses @grant none

--- other/animmex-fix.user.js
+++ other/animmex-fix.user.js
@@ -6,14 +6,35 @@
 // @include     http://amx.*.xyz/*
 // @include     https://www.animmex.net/*
 // @include     http://www.animmex.net/*
-// @version     1.0
-// @grant       GM_addStyle
+// @version     1.1
+// @grant       none
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
 'use strict';
 
+function addStyle(css) {
+  const head = document.head || document.getElementsByTagName('head')[0];
+  if (!head) return;
+  
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  
+  if (style.textContent) {
+    style.textContent = css;
+  } else if (style.innerHTML) {
+    style.innerHTML = css;
+  }/* else if (style.innerText) {
+    style.innerText = css;
+  }*/ else {
+    return;
+  }
+  
+  head.appendChild(style);
+};
+
+
 // only has effect under amx.*.xyz
-GM_addStyle(`
+addStyle(`
 .video-js {
     width: 100% !important;
     height: 100% !important;
@@ -25,3 +46,6 @@ const voteBox = document.getElementsByClassName('vote-box');
 if (voteBox.length > 0) { // will only be true under animmex.net
   voteBox[0].parentElement.style.width = '100%';
 }
+
+// we want @grant none so we can assign this to `window` and block ze popups
+window.open = function(){};
