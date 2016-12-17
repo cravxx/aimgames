dcd66c54d6ccf64dac4d968aaed7467aa575948a
chrishansen69 <cadrekucra@gmx.com>
2016-12-17 17:29:09 -0200

    clear unused crap before it causes problems

--- bin/chatboxplusplus.compiled.user.js
+++
@@ -1,2 +0,0 @@
-'use strict';alert("This script is broken, please switch to Chatbox++ v2");
-'use strict';alert("This script is broken, please switch to Chatbox++ v2");
--- bin/loginlogout.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name        LoginLogout
-// @namespace   greasy_af
-// @include     http://aimgames.forummotion.com/chatbox/index.forum
-// @version     1
-// @grant       none
-// ==/UserScript==
-'use strict';setInterval(function(){chatbox.a?document.getElementById("message").onkeypress=function(a){13==(a.keyCode?a.keyCode:a.which)&&chatbox.disconnect()}:chatbox.connect()},100);
