5fa156ca0c698005ef52be5e5971eb08cae1b913
chrishansen69 <cadrekucra@gmx.com>
2016-12-17 14:03:19 -0200

    cleaner AddUserscriptLines

--- build/src/br/com/chrishansen/aimgames/builder/AddUserscriptLines.java
+++ build/src/br/com/chrishansen/aimgames/builder/AddUserscriptLines.java
@@ -2,52 +2,70 @@ package br.com.chrishansen.aimgames.builder;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddUserscriptLines {
-    public static void main(final String[] args) throws IOException, InterruptedException {
+    public static void main(final String[] args) throws IOException {
 
         if (args.length < 3)
             throw new RuntimeException("not enough arguments to program");
 
-        final int linesToKeep = Integer.valueOf(args[2]);
+        //final int linesToKeep = Integer.valueOf(args[2]);
 
         final String lineFrom = args[1]; //get only argument 2
         System.out.println(lineFrom);
+        
         final File f = new File("./" + lineFrom);
         System.out.println("it exists? " + f.exists());
+        
         // read lines to keep
-        final BufferedReader reader = new BufferedReader(new FileReader(f));
-        String keepLine = "";
-        for (int i = 0; i < linesToKeep; i++)
-            keepLine = keepLine + reader.readLine() + "\r\n";
-        reader.close();
-        System.out.println(keepLine);
 
+        List<String> result = new ArrayList<>();
+        
+        try (BufferedReader br = getReader(f)) {
+            for (;;) {
+                String line = br.readLine();
+                if (line == null)
+                    break;
+                result.add(line);
+                if (line.trim().equals("// ==/UserScript=="))
+                    break;
+            }
+        }
+        
+        
         // add kept lines
         final String lineTo = args[0]; //get only argument 1
         System.out.println("lineto " + lineTo);
+        
         final File fTo = new File("./" + lineTo);
+
         if (!fTo.exists())
             fTo.createNewFile();
-        final FileInputStream fis = new FileInputStream(fTo);
-        final BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-        String result = "";
-        String resultLine = "";
-        while ((resultLine = br.readLine()) != null)
-            result = result + resultLine;
-        br.close();
-
-        result = keepLine + result;
+        
+        try (BufferedReader br = getReader(fTo)) {
+            for (;;) {
+                String line = br.readLine();
+                if (line == null)
+                    break;
+                result.add(line);
+            }
+        }
 
         fTo.delete();
+        
         final FileOutputStream fos = new FileOutputStream(fTo);
-        fos.write(result.getBytes());
+        fos.write(String.join("\r\n", result).getBytes("UTF-8"));
         fos.flush();
         fos.close();
+    }
+
+    private static BufferedReader getReader(final File f) throws FileNotFoundException {
+        return new BufferedReader(new FileReader(f));
     }
 }
