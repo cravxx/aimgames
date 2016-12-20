0522f10bcd0531d896ed0de84b4b67e037e23d78
Appveyor Automated Build <bbbbbbenes@outlook.com.br>
2016-12-20 18:19:09 +0000

    Automated Appveyor build at 2016-12-20, 06:19 PM [skip ci]

--- bin/wikia-bypass.min.user.js
+++ bin/wikia-bypass.min.user.js
@@ -1,11 +1,15 @@
 // ==UserScript==
-// @name        Direct external links for Wikia
-// @description Bypass adds when clicking external links in Wikia pages
-// @namespace   hansen-i-nate24214@gmail.com
-// @match       http://*.wikia.com/*
-// @match       https://*.wikia.com/*
-// @version     1.1
-// @grant       none
-// @license     MIT License (Expat); opensource.org/licenses/MIT
+// @name          Wikia width fix / direct external links
+// @description   Fixes page width for wikia.com and bypasses ads for external links
+// @namespace     http://userstyles.org
+// @author        chrishansen69
+// @include       http://wikia.com/*
+// @include       https://wikia.com/*
+// @include       http://*.wikia.com/*
+// @include       https://*.wikia.com/*
+// @grant         none
+// @run-at        document-end
+// @version       1.2
 // ==/UserScript==
-'use strict';$(".external.text.exitstitial").a("click");
+'use strict';if("undefined"!=typeof GM_addStyle)GM_addStyle("#WikiaMainContent{width: 100%;}table.nowraplinks.collapsible{width: 100%;}");else if("undefined"!=typeof PRO_addStyle)PRO_addStyle("#WikiaMainContent{width: 100%;}table.nowraplinks.collapsible{width: 100%;}");else if("undefined"!=typeof addStyle)addStyle("#WikiaMainContent{width: 100%;}table.nowraplinks.collapsible{width: 100%;}");else{var a=document.createElement("style");a.type="text/css";a.appendChild(document.createTextNode("#WikiaMainContent{width: 100%;}table.nowraplinks.collapsible{width: 100%;}"));
+(document.head||document.getElementsByTagName("head")[0]||document.documentElement).appendChild(a)}$(".external.text.exitstitial").a("click");
