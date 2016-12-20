9d7e05041dd603d2ea8d4766cc9ab53166863d19
rafa1231518 <chris331hansen@outlook.com>
2016-12-20 16:16:05 -0200

    merge wikia bypass and width fix

--- other/wikia-bypass.user.js
+++ other/wikia-bypass.user.js
@@ -1,14 +1,36 @@
 // ==UserScript==
-// @name        Direct external links for Wikia
-// @description Bypass adds when clicking external links in Wikia pages
-// @namespace   hansen-i-nate24214@gmail.com
-// @match       http://*.wikia.com/*
-// @match       https://*.wikia.com/*
-// @version     1.1
-// @grant       none
-// @license     MIT License (Expat); opensource.org/licenses/MIT
+// @name          Wikia width fix / direct external links
+// @description   Fixes page width for wikia.com and bypasses ads for external links
+// @namespace     http://userstyles.org
+// @author        chrishansen69
+// @include       http://wikia.com/*
+// @include       https://wikia.com/*
+// @include       http://*.wikia.com/*
+// @include       https://*.wikia.com/*
+// @grant         none
+// @run-at        document-end
+// @version       1.2
 // ==/UserScript==
+
+// document-start w/ timeout or document-end???
 'use strict';
 
-// remove click listeners for this external links
-$('.external.text.exitstitial').off('click');
+const css = "#WikiaMainContent{width: 100%;}table.nowraplinks.collapsible{width: 100%;}";
+if (typeof GM_addStyle != "undefined") {
+	GM_addStyle(css);
+} else if (typeof PRO_addStyle != "undefined") {
+	PRO_addStyle(css);
+} else if (typeof addStyle != "undefined") {
+	addStyle(css);
+} else {
+	var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	var head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+	head.appendChild(node);
+}
+
+
+//setTimeout(function() { // remove click listeners for this external links
+	$('.external.text.exitstitial').off("click");
+//}, 1500); // wait a bit because ????
