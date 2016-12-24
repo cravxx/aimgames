295d6e99863341d581abcf4335caf1f2e33dfa01
rafa1231518 <chris331hansen@outlook.com>
2016-12-23 23:00:59 -0200

    Update fact.user.js

--- other/fact.user.js
+++ other/fact.user.js
@@ -4,13 +4,14 @@
 // @namespace   a@nus.avi
 // @include     http://mods.factorio.com/*
 // @include     https://mods.factorio.com/*
-// @version     1.0
+// @version     1.4
 // @grant       GM_xmlhttpRequest
+// @grant       GM_addStyle
 // ==/UserScript==
 
 // <iframe id="iframe"></iframe>
 
-const msg = window.alert;//msg function
+const msg = window.alert.bind(window);//msg function
 
 const tframe = document.createElement('iframe');
 tframe.setAttribute('id', 'tiframe');
@@ -32,26 +33,24 @@ function downloadMod(modUrl) {
       onload: function(response) {
         const data = JSON.parse(response.responseText);
         if (data === null) {
-          msg('Could not contact API');
+          msg('Could not contact API' + ' url: ' + param);
         } else if (!data.success) {
-          msg('[failed] ' + data.error);
+          msg('[failed] ' + data.error + ' url: ' + param);
         } else if (!data.url) {
-          msg('Unexpected error');
+          msg('Unexpected error' + ' url: ' + param);
         } else {
-          tframe.setAttribute(src, data.url);
+          tframe.setAttribute('src', data.url);
         }
       }
     });
   }
 }
 
-const footers = document.getElementsByClassName('mod-card-footer');
-
-for (let i = 0, len = footers.length; i < len; i++) {
-  const f = footers[i];
+function handleNode(f) {
   
   const but1 = document.createElement('div');
   but1.setAttribute('class', 'mod-card-buttons');
+  but1.setAttribute('style', 'margin-right: 5px;');
   const but2 = document.createElement('div');
   but2.setAttribute('class', 'btn mod-download-button btn-download btn-disabled');
   const but3 = document.createElement('a');
@@ -59,8 +58,10 @@ for (let i = 0, len = footers.length; i < len; i++) {
   const but4 = document.createElement('span');
   but4.appendChild(document.createTextNode('DOWNLOAD THROUGH API'));
 
+  // years of javascript on top of javascript teaches you this kind of sync-async stuff
+  const staticref = f.children[1].firstChild.firstChild.href;
   but1.addEventListener("click", () => {
-    downloadMod(f.children[1].firstChild.firstChild.href);
+    downloadMod(staticref);
   }); 
 
   but3.appendChild(but4);
@@ -69,3 +70,24 @@ for (let i = 0, len = footers.length; i < len; i++) {
   
   f.insertBefore(but1, f.children[1]);
 }
+
+GM_addStyle(`
+@keyframes cccnodeInserted {  
+    from {  
+        outline-color: #fff; 
+    }
+    to {  
+        outline-color: #000;
+    }  
+}
+.mod-card-footer {
+    animation-duration: 0.01s;
+    animation-name: cccnodeInserted;
+}
+`);
+
+document.addEventListener('animationstart', function(event){
+  if (event.animationName == 'cccnodeInserted'){
+    handleNode(event.target);
+  }
+}, true);
