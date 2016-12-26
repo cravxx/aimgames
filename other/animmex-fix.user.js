3a8585ea3d6bddc91fb2ee41c8b2bf9157fe7658
rafa1231518 <chris331hansen@outlook.com>
2016-12-25 22:44:55 -0200

    animmex fix now saves current time

--- other/animmex-fix.user.js
+++ other/animmex-fix.user.js
@@ -6,31 +6,37 @@
 // @include     http://amx.*.xyz/*
 // @include     https://www.animmex.net/*
 // @include     http://www.animmex.net/*
-// @version     1.2
-// @grant       none
+// @version     1.4
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_addStyle
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
 'use strict';
 
-function addStyle(css) {
-  const head = document.head || document.getElementsByTagName('head')[0];
-  if (!head) return;
-  
-  const style = document.createElement('style');
-  style.type = 'text/css';
-  
-  try {
-    style.textContent = css;
-  } catch(e) {
-    style.innerHTML = css;
-  }
-  
-  head.appendChild(style);
-};
+const _unsafeWindow = typeof unsafeWindow != 'undefined' ? unsafeWindow : window.wrappedJSObject;
+
+if (typeof GM_addStyle == 'undefined') {
+  var GM_addStyle = function(css) {//not let or const
+    const head = document.head || document.getElementsByTagName('head')[0];
+    if (!head) return;
+
+    const style = document.createElement('style');
+    style.type = 'text/css';
+
+    try {
+      style.textContent = css;
+    } catch(e) {
+      style.innerHTML = css;
+    }
+
+    head.appendChild(style);
+  };
+}
 
 
 // only has effect under amx.*.xyz
-addStyle(`
+GM_addStyle(`
 .video-js, #video_1 {
     width: 100% !important;
     height: 100% !important;
@@ -43,5 +49,32 @@ if (voteBox.length > 0) { // will only be true under animmex.net
   voteBox[0].parentElement.style.width = '100%';
 }
 
-// we want @grant none so we can assign this to `window` and block ze popups
-window.open = function(){};
+const videos = document.getElementsByTagName('video');
+
+if (videos.length!==0){
+  const videoId = videos[0].getAttribute('poster');
+  videos[0].currentTime=GM_getValue(videoId, 0);
+  
+  window.addEventListener('beforeunload', function() {
+    GM_setValue(videoId, videos[0].currentTime);
+  });
+}
+
+function browserContext() {
+  /***** DO NOT REFERENCE EXTERNAL VARIABLES FROM THIS FUNCTION *****/
+  'use strict';
+  const noopfn = function(){};
+  window.open = noopfn;
+  
+  window.init = noopfn;//not native; animmex thing
+  try{
+  Object.defineProperty(window, 'init', {
+    configurable:true,
+    set:noopfn,
+    get:()=>noopfn
+  });
+  }catch(e){}
+}
+
+window.eval(browserContext.toSource() +';browserContext()');
+
