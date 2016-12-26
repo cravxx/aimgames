e10838e7562d5dd50fb7719e749768207bfc9c09
Appveyor Automated Build <bbbbbbenes@outlook.com.br>
2016-12-26 03:02:49 +0000

    Automated Appveyor build at 2016-12-26, 03:02 AM [skip ci]

--- bin/chatboxplusplus.min.user.js
+++ bin/chatboxplusplus.min.user.js
@@ -196,3 +196,12 @@
 // @grant       none
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
+// ==UserScript==
+// @name        Chatbox++
+// @description Adds a number of 'universal' enhancements for the AIM Games chatbox. Warning: This script is still in active development and may contain bugs!
+// @namespace   the_thrasher@gmail.com
+// @include     http://aimgames.forummotion.com/
+// @version     1.43
+// @grant       none
+// @license     MIT License (Expat); opensource.org/licenses/MIT
+// ==/UserScript==
