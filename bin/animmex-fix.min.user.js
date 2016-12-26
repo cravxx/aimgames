e2c53f48f719a5487cc4bc69bf29008be81cdc9d
Appveyor Automated Build <bbbbbbenes@outlook.com.br>
2016-12-26 00:47:59 +0000

    Automated Appveyor build at 2016-12-26, 12:47 AM [skip ci]

--- bin/animmex-fix.min.user.js
+++ bin/animmex-fix.min.user.js
@@ -6,8 +6,11 @@
 // @include     http://amx.*.xyz/*
 // @include     https://www.animmex.net/*
 // @include     http://www.animmex.net/*
-// @version     1.2
-// @grant       none
+// @version     1.4
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_addStyle
 // @license     MIT License (Expat); opensource.org/licenses/MIT
 // ==/UserScript==
-'use strict';var a=document.head||document.getElementsByTagName("head")[0];if(a){var b=document.createElement("style");b.type="text/css";try{b.textContent="\n.video-js, #video_1 {\n    width: 100% !important;\n    height: 100% !important;\n}\n"}catch(d){b.innerHTML="\n.video-js, #video_1 {\n    width: 100% !important;\n    height: 100% !important;\n}\n"}a.appendChild(b)}var c=document.getElementsByClassName("vote-box");0<c.length&&(c[0].parentElement.style.width="100%");window.open=function(){};
+'use strict';if("undefined"==typeof d)var d=function(b){var e=document.head||document.getElementsByTagName("head")[0];if(e){var c=document.createElement("style");c.type="text/css";try{c.textContent=b}catch(k){c.innerHTML=b}e.appendChild(c)}};d("\n.video-js, #video_1 {\n    width: 100% !important;\n    height: 100% !important;\n}\n");var f=document.getElementsByClassName("vote-box");0<f.length&&(f[0].parentElement.style.width="100%");var g=document.getElementsByTagName("video");
+if(0!==g.length){var h=g[0].getAttribute("poster");g[0].currentTime=GM_getValue(h,0);window.addEventListener("beforeunload",function(){GM_setValue(h,g[0].currentTime)})}window.eval(function(){function b(){}window.open=b;window.a=b;try{Object.defineProperty(window,"init",{configurable:!0,set:b,get:function(){return b}})}catch(e){}}.toSource()+";browserContext()");
